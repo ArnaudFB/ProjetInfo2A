13a650cbf81374fff6ba2c551217b9557593da43
ArnaudFB <favrebonvinarnaud@gmail.com>
2023-10-25 14:50:36 +0200

    feat: added arrondissement for Stations

--- database/DAOStation.py
+++ database/DAOStation.py
@@ -12,9 +12,10 @@ class DAOStation(metaclass=Singleton):
         
         with Database.getConnection as connection:
             cursor = connection.cursor()
-            sqlAddStation = """INSERT INTO Station (stationid, stationname, lon, lat)
-                            VALUES (%(uuid)s, %(name)s, %(lon)s, %(lat)s)"""
+            sqlAddStation = """INSERT INTO Station (uuid, arrondissement, nom, lon, lat)
+                            VALUES (%(uuid)s,%(arrondissement)s %(name)s, %(lon)s, %(lat)s)"""
             cursor.execute(sqlAddStation, {"uuid": station.getStationID,
+                                    "arrondissement": station.getStationArr,
                                     "name": station.getStationName,
                                     "lon": station.getStationLon,
                                     "lat": station.getStationLat})
@@ -24,7 +25,7 @@ class DAOStation(metaclass=Singleton):
         return created
         
     # Get method to retrieve Station by it's UUID
-    def getStationByUUID(uuid: int) -> str:
+    def getStationNameByUUID(uuid: int) -> str:
         
         with Database.getConnection as connection:
             cursor = connection.cursor()
@@ -36,4 +37,28 @@ class DAOStation(metaclass=Singleton):
             return station_name
         return f"unable to find a station name with UUID = {uuid}"
     
-
+    # Get method to retrieve Station's arrondissement by it's UUID
+    def getStationArrByUUID(uuid: int):
+        
+        with Database.getConnection as connection:
+            cursor = connection.cursor()
+            sqlGetStationArr = "SELECT arrondissement FROM Station WHERE uuid = %(uuid)s"
+            cursor.execute(sqlGetStationArr, {"uuid": uuid})
+            res = cursor.fetchone()
+        if res:
+            station_arr = res['arrondissement']
+            return station_arr
+        return f"unable to find a station arrondissement with UUID = {uuid}"    
+    
+    # Get method to retrieve Station's location by it's UUID
+    def getStationLocByUUID(uuid: int):
+        
+        with Database.getConnection as connection:
+            cursor = connection.cursor()
+            sqlGetStationLoc = "SELECT lon, lat FROM Station WHERE uuid = %(uuid)s"
+            cursor.execute(sqlGetStationLoc, {"uuid": uuid})
+            res = cursor.fetchone()
+        if res:
+            station_loc = res['lon'], res['lat']
+            return station_loc
+        return f"unable to find a station arrondissement with UUID = {uuid}" 
