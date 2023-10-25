aca9cbbff78988e725d38e06493ee5c5ea76b7db
coline <coline.brier@eleve.ensai.fr>
2023-10-25 16:13:20 +0200

    petits détails DAORecords et tests

--- database/DAORecord.py
+++ database/DAORecord.py
@@ -46,7 +46,7 @@ class DAORecord(metaclass=Singleton):
             return records
         return f"unable to find a record for station {station_uuid} between {date_start} and {date_end}"
 
-    def getVarByDate(self, date_start : datetime.datetime, date_end : datetime.datetime) -> list[int]:
+    def getVarSationByDate(self, date_start : datetime.datetime, date_end : datetime.datetime) -> list[tuple]:
         
         with Database.getConnection as connection:
             cursor = connection.cursor()
