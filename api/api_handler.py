a7609dcd241843beb43b5762200ed8d0bb240940
ArnaudFB <favrebonvinarnaud@gmail.com>
2023-10-25 11:48:39 +0200

    feat: -Implement first feature
    todo:
    -Improve the output (design)

--- api/api_handler.py
+++ api/api_handler.py
@@ -1,19 +1,29 @@
 # Import necessary modules
 import requests
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Query
 import uvicorn
 import json
 import httpx
 from data.Location import Location
+from service.StationManager import StationManager
 
 
 # Creating API
 app = FastAPI()
-         
-@app.get("/fonctionnalite-1")    
-async def getNearestStation(loc : Location):
+
+@app.get("/fonctionnalite-1/")    
+async def getNearestStation(user_location: str = Query((48.8563199, 2.31345367))):
     
-    station = getStations().getAvailableStations().getNearestStation(loc)
+    user_location = tuple(map(float, user_location.split(',')) if ',' in user_location else (48.8563199, 2.31345367))
+
+    # Base URL for the app
+    BASE_URL = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/exports/json"
+    
+    station = await StationManager(BASE_URL).getStations()
+    
+    station = StationManager.getAvailableStation(station)
+    
+    station = StationManager.getNearestStation(station, user_location)
     
     return station
     
